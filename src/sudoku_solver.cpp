f2d6263bf68435ffd95558be7efac9d5d486933e
BenQuickDeNN <benquickdenn@foxmail.com>
2019-09-23 21:51:46 +0800

    initialize mainfile

---
+++ src/sudoku_solver.cpp
@@ -0,0 +1,28 @@
+/*******************************************
+ * @title   Sudoku Solver
+ * @brief   A project that automatically
+ * solves sudoku.
+ * @author  Bin Qu
+ * @date    2019.9.23
+ * @copyright   You can edit and remodify
+ * this project.
+*******************************************/
+
+int main()
+{
+    /* load sudoku file */
+
+    /* initialize global vars */
+
+    /* compute the number of blocks */
+
+    /* generate candidates */
+
+    /* fill lattices */
+
+    /* excluding */
+
+    /* output the solution */
+
+    return 0;
+}
