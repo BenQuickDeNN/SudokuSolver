47f424819be196b5868519fab2040f24ed84d053
BenQuickDeNN <2413269761@qq.com>
2019-09-24 17:42:29 +0800

    add block module

--- include/element.cxx
+++ include/element.cxx
@@ -7,15 +7,99 @@
  * this file.
 *******************************************/
 
-#include <cstdio>
 #include <cstdlib>
 #include <map>
 
-namespace sudoku_solver
+/// namespace sudoku solver
+namespace sds
 {   
     class Grid
     {
+    private:
+        /**
+         * @brief element lattice
+        */
+        char* lattices;
+        /**
+         * @brief candidated digit
+         * mask
+        */
+        bool* mask;
+        /**
+         * @brief grid size length
+        */
+        unsigned int length;
+        /**
+         * @brief length of block
+        */
+        unsigned int blocklength;
     public:
+        const unsigned int& Length() {return length;}
+        unsigned int Size() {return length * length;}
+        const unsigned int& BlockLength()
+        { return blocklength;}
+        unsigned int BlockSize()
+        { return blocklength * blocklength; }
         
+        /**
+         * @brief map block address to lattice
+         * address
+        */
+        char& Block(const unsigned int& i,
+            const unsigned int& j)
+        { return lattices[i * BlockSize() + 
+            j * blocklength]; }
+        
+        /**
+         * @brief overload "(i, j)"
+         * @return element(i, j)
+        */
+        char& operator()(const unsigned int& i,
+            const unsigned int& j)
+        { return lattices[i * length + j]; }
+
+        /**
+         * @brief overload "(i, j)"
+         * @return if the digit is available
+        */
+        bool operator()(const unsigned int& i,
+            const unsigned int& j, const char& digit)
+        { return mask[i * length * length + j *
+            length + (digit - 1)]; }
+
+        /**
+         * @brief constructor that allocates
+         * memory
+        */
+        Grid(const unsigned int& length,
+            const unsigned int& blocklength)
+            :length(length), blocklength(blocklength)
+        {
+            /* allocate memory for lattices */
+            lattices = (char*)std::malloc(
+                Size() * sizeof(char));
+            /* set zero */
+            for (int i = 0; i < Size(); i++)
+                lattices[i] = 0;
+
+            /* allocate memory for mask */
+            mask = (bool*)std::malloc(Size() *
+                length * sizeof(bool));
+            /* set true */
+            for (int i = 0; i < Size() * length;
+                i++)
+                mask[i] = true;
+        }
+
+        /**
+         * @brief deconstructor that frees
+         * memory
+        */
+        ~Grid()
+        {
+            /* free memory */
+            std::free(lattices);
+            std::free(mask);
+        }
     };
 }
--- src/sudoku_solver.cpp
+++ src/sudoku_solver.cpp
@@ -38,6 +38,28 @@ int main()
 /* test for grid */
 void test1()
 {
-    printf("start test1...\r\n");
-    
+    std::printf("start test1...\r\n");
+
+    sds::Grid grid(9, 3);
+
+    std::printf("print grid lattices:\r\n");
+    for (int i = 0; i < grid.Length(); i++)
+    {
+        for (int j = 0; j < grid.Length(); j++)
+            std::printf("%d\t", grid(i, j));
+        std::printf("\r\n");
+    }
+
+    std::printf("print digit mask:\r\n");
+    for (int i = 0; i < grid.Length(); i++)
+    {
+        for (int j = 0; j < grid.Length(); j++)
+        {   
+            for (char digit = 1; digit <= 
+                grid.Length(); digit++)
+                std::printf("%d\t", grid(i, j, digit));
+            std::printf("\r\n");
+        }
+        std::printf("\r\n");
+    }
 }
